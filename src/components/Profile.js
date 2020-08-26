0c2b589dd4685eb0ef509525b0f4dddcb8ffc6e5
yuy.hirohiro@gmail.com <yuy.hirohiro@gmail.com>
2020-08-26 21:33:48 +0900

    不要なファイルを削除

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -4,7 +4,7 @@ import './Profile.css'
 function Profile() {
     return (
         <div className="profile">
-            <img alt="" src="https://ja.gravatar.com/userimage/126288691/18432f3b22b866e0bb7b59d466f325bd.png"/>
+            <img className="profile-image" alt="" src="https://ja.gravatar.com/userimage/126288691/18432f3b22b866e0bb7b59d466f325bd.png"/>
             <div className="profile-link">
                 <ul>
                     <li><a href="https://twitter.com/yuya_hirooka">Twitter</a></li>
