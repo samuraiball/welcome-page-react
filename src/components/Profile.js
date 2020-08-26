275ee68aed87315c8e20b274674876080e19aef8
yuy.hirohiro@gmail.com <yuy.hirohiro@gmail.com>
2020-08-26 20:16:39 +0900

    create simple profile

---
+++ src/components/Profile.js
@@ -0,0 +1,19 @@
+import React from "react";
+import './Profile.css'
+
+function Profile() {
+    return (
+        <div className="profile">
+            <img className="profile-image" src="https://ja.gravatar.com/userimage/126288691/18432f3b22b866e0bb7b59d466f325bd.png"/>
+            <div className="profile-link">
+                <ul>
+                    <li><a href="https://twitter.com/yuya_hirooka">Twitter</a></li>
+                    <li><a href="https://github.com/samuraiball">GitHub</a></li>
+                    <li><a href="https://yuya-hirooka.hatenablog.com/">はてなブログ</a></li>
+                </ul>
+            </div>
+        </div>
+    )
+}
+
+export default Profile
