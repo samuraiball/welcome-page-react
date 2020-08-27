ad78630f079726533f663d5757214cc7ee26afa3
yuy.hirohiro@gmail.com <yuy.hirohiro@gmail.com>
2020-08-27 23:43:39 +0900

    add about me

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -4,8 +4,20 @@ import './Profile.css'
 function Profile() {
     return (
         <div className="profile">
-            <img className="profile-image" alt="" src="https://ja.gravatar.com/userimage/126288691/18432f3b22b866e0bb7b59d466f325bd.png"/>
-            <div className="profile-link">
+            <img className="image" alt="" src="https://ja.gravatar.com/userimage/126288691/18432f3b22b866e0bb7b59d466f325bd.png"/>
+            <div className="about-me">
+                <div className="section-title">
+                   About Me
+                </div>
+                <p>
+                    Software Engineer at Uzabase
+                </p>
+
+            </div>
+            <div className="links">
+                <div className="section-title">
+                    Links
+                </div>
                 <ul>
                     <li><a href="https://twitter.com/yuya_hirooka">Twitter</a></li>
                     <li><a href="https://github.com/samuraiball">GitHub</a></li>
