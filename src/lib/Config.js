06533ac7b622ddeb7674d29acf3a543c38c91c2e
yuy.hirohiro@gmail.com <yuy.hirohiro@gmail.com>
2020-09-10 23:25:01 +0900

    不要なコンソールログを削除

--- src/lib/Config.js
+++ src/lib/Config.js
@@ -1,6 +1,5 @@
 const Config = () => {
     if (process.env.NODE_ENV === 'production') {
-        console.log(process.env.REACT_APP_PROD_WELCOME_PAGE_API_ENDPOINT)
         return {
             welcomePageEndpoint: process.env.REACT_APP_PROD_WELCOME_PAGE_API_ENDPOINT
         }
