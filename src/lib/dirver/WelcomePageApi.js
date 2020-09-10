7838ceb937354596c5c36391fcfd5101f3ffd4d5
yuy.hirohiro@gmail.com <yuy.hirohiro@gmail.com>
2020-09-10 22:56:49 +0900

    fix endpoint

--- src/lib/dirver/WelcomePageApi.js
+++ src/lib/dirver/WelcomePageApi.js
@@ -3,7 +3,7 @@ import axios from "axios";
 class WelcomePageApi {
     async fetchBlogs() {
         return axios(
-            "http://localhost:8081/hatena/entries",
+            "https://welcome-page-api.herokuapp.com/hatena/entries",
             {
                 headers: {
                     "Access-Control-Allow-Origin": "*",
